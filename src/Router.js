9a0430f8d00c7a9ef20fc497233132c97105b918
azezcodes <aziz@swatitech.com>
2023-11-01 14:41:03 +0500

    wrapped route with React Suspense for lazy load

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,16 @@
-import React, { Fragment } from 'react';
-import Home from './View/Home';
-
+import React  from 'react';
+ 
+import { Suspense,lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
+import Splash from './View/Splash';
+const Home = lazy(()=>import('./View/Home'));
 const AppRouter = () => {
   return (
-    <Fragment>
+    <Suspense fallback={<Splash />}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </Fragment>
+    </Suspense>
   );
 };
 
