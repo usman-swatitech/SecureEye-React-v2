1e70bfc776e2d344ec6e8b1bf80e143a387e33ac
Kashif <muhammadkashif@swatitech.com>
2023-11-01 14:53:19 +0500

    update code

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,11 @@
 import React  from 'react';
- 
-import { Suspense,lazy } from 'react';
+import Home from './View/Home';
 import { Routes, Route } from 'react-router-dom';
-import Splash from './View/Splash';
-const Home = lazy(()=>import('./View/Home'));
 const AppRouter = () => {
   return (
-    <Suspense fallback={<Splash />}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </Suspense>
   );
 };
 
--- src/View/Home.jsx
+++ src/View/Home.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from 'react';
+import SplashScreen from './Splash';
+
 const App = () => {
- 
-  
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 2000); // Display for 3 seconds
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-   <h1>Home Layout</h1>
+    <div>
+      {showSplash ? (
+        <SplashScreen />
+      ) : (
+        <Fragment>
+            <h1>Home screen</h1>
+        </Fragment>
+      )}
+    </div>
   );
 };
 
