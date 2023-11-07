72aa7d29c6fd4e0365979679a0674b8f6c0df7a0
Kashif <muhammadkashif@swatitech.com>
2023-11-07 15:11:13 +0500

    employee comp add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
 import "./aziz.css";
 import "./usman.css";
+import "./index.css";
 import Routes from "./Router";
 import { AppContextProvider } from "./context/Context";
 const App = () => {
