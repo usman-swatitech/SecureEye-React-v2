8b391dc4bc60efa4ffbc491f3a0a4de89be48009
azezcodes <aziz@swatitech.com>
2023-11-07 15:14:40 +0500

    imported table component, setup oHomeLayout, fixed single screen, and cards

--- src/Layouts/SingleScreenLayout.jsx
+++ src/Layouts/SingleScreenLayout.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import video from "../assets/mp4/test.mp4";
+import Table from "../Component/Common/Table";
+import frame from "../Component/frames/frame.png";
 const SingleScreenLayout = () => {
   return (
-    <section className="h-auto w-100 d-flex flex-column position-relative">
-      <div
+    <section className="h-auto w-100 d-flex flex-column position-relative gap-2">
+      <div className="single-camera-frame crads-layout">
+        <img src={frame} al="frame" className="single-camera-frame" />
+        <div className="frame-inner">
+          <label className="text-white ms-3 pb-1">CAMERA 01</label>
+          <video autoPlay loop muted className="w-100">
+            <source src={video} />
+          </video>
+        </div>
+      </div>
+      <label
         style={{
-          postion: "relative",
-          width: "400px",
-          height: "200px",
+          fontSize: "18px",
+          fontWeight: "200",
+          lineHeight: "22px",
+          color: "var(--main-white-color)",
         }}
       >
-        <video
-          autoPlay
-          muted
-          loop
-          className="position-absolute w-100 h-100"
-          style={{ objectFit: "contain" }}
-        >
-          <source src={video} />
-        </video>
+        Employee Information
+      </label>
+      <div className="table-layout-2">
+        <Table />
       </div>
     </section>
   );
