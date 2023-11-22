2e534084ef39cfce4e35567259390b61ff7db309
Kashif <muhammadkashif@swatitech.com>
2023-11-22 11:46:30 +0500

    update time code

--- src/Component/Common/Timer.jsx
+++ src/Component/Common/Timer.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 const Timer = () => {
+  const formatTime = (date) => {
+    const hours = date.getHours().toString().padStart(2, "0");
+    const mins = date.getMinutes().toString().padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+    return `${hours}:${mins}:${seconds}`;
+  };
+
   const [time, setTime] = useState(formatTime(new Date()));
 
   useEffect(() => {
@@ -11,14 +18,9 @@ const Timer = () => {
     return () => clearInterval(timerId);
   }, []);
 
-  function formatTime(date) {
-    const hours = date.getHours().toString().padStart(2, "0");
-    const mins = date.getMinutes().toString().padStart(2, "0");
-    const seconds = date.getSeconds().toString().padStart(2, "0");
-    return `${hours}:${mins}:${seconds}`;
-  }
-
-  return <div className="time_stamp">{time}</div>;
+  return (
+  <div className="time_stamp">{time}</div>
+  );
 };
 
 export default Timer;
